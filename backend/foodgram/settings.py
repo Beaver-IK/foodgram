ca2d62c55049a16b0ffab8cdb9fc37a1a77d8da2
Ivan Kontsov <ivan.kontsov@gmail.com>
2025-02-21 02:41:24 +0300

    add user model

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-v6i8bu(i58d@9gl8kk@b=c-^=*nl$+)-$rai9s6q-l5$^q7(s4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'django_filters',
+    'djoser',
+    'api.apps.ApiConfig',
+    'users.apps.UsersConfig',
+    'cart.apps.CartConfig',
+    'ingridient.apps.IngredientConfig',
+    'recipe.apps.RecipeConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,118 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.utils import timezone
+from django.core.mail import send_mail
 
-# Create your models here.
+from users import constants as c
+
+
+class User(AbstractUser):
+    """Кастомная модель пользователя."""
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        'Имя пользователя',
+        max_length=c.LEN_USERNAME,
+        unique=True,
+        help_text=(
+            'Обязательное поле. Не более 150 символов. '
+            'Можно использовать буквы, цифры и спецсимволы: @/./+/-/_'
+        ),
+        validators=[username_validator],
+        error_messages=dict(
+            unique='Пользователь с таким именем пользователя уже существует.'
+        ),
+    )
+    first_name = models.CharField('Имя', max_length=c.LEN_FIRSTNAME,)
+    last_name = models.CharField('Фамилия', max_length=c.LEN_LASTNAME)
+    email = models.EmailField('Email', max_length=c.LEN_EMAIL, unique=True)
+    is_staff = models.BooleanField(
+        'Сотрудник организации',
+        default=False,
+        help_text='Определяет, может ли пользоваться админкой.',
+    )
+    is_active = models.BooleanField(
+        'Активен',
+        default=True,
+        help_text=(
+            'Вместо удадения пользователя, можно его сделать неактивным.'
+        ),
+    )
+    date_joined = models.DateTimeField('Дата посещения', default=timezone.now)
+    avatar = models.ImageField(
+        'Аватар',
+        upload_to='users/avatars/',
+        blank=True,
+        null=True
+    )
+    subscriptions = models.ManyToManyField(
+        'Подписки',
+        symmetrical=False,
+        related_name='subscribers',
+        blank=True
+    )
+    cart = models.ForeignKey(
+        'Cart',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='user_cart',
+        verbose_name='Корзина'
+    )
+    favourites = models.ManyToManyField(
+        'Recipe',
+        blank=True,
+        related_name='favorited_by',
+        verbose_name='Избранное'
+    )
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.username
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def get_full_name(self):
+        """
+        Возвращает строку с именем и фамилией.
+        """
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        """Возвращает только имя."""
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Отправка письма пользователю."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+    @property
+    def is_subscribed(self):
+        pass
+
+    @property
+    def is_favorite(self):
+        pass
+
+    @classmethod
+    def already_use(cls, kwargs):
+        username = kwargs.get('username')
+        email = kwargs.get('email')
+        errors = dict()
+        if cls.objects.filter(username=username).exclude(email=email).exists():
+            errors['username'] = f'Username {username} уже используется.'
+        if cls.objects.filter(email=email).exclude(username=username).exists():
+            errors['email'] = f'Email {email} уже используется.'
+        return errors
